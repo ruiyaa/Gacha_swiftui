942849cd86ce6d84ba5519f45d8cce9c57eb9db8
ruiyaa <nini2525min@gmail.com>
2025-07-11 20:57:06 +0900

    Initial Commit

---
+++ Gacha_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gacha_swiftui
+//
+//  Created by tanaka niko on 2025/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gacha_swiftui/Gacha_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gacha_swiftuiApp.swift
+//  Gacha_swiftui
+//
+//  Created by tanaka niko on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Gacha_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
